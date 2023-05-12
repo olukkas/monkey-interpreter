6dd991f9709e152db5526a35c9bafc6aaced818c
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-12 20:46:01 -0300

    refact(ast): refactoring ast package

---
+++ ast/let_statement.go
@@ -0,0 +1,39 @@
+package ast
+
+import (
+	"bytes"
+	"monkey/token"
+)
+
+type LetStatement struct {
+	Token token.Token // the token.Let
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type Identifier struct {
+	Token token.Token // the token.Ident
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
