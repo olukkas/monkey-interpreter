d0b864af950adada4c7e8b94a931c94078431129
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-05-18 20:05:20 -0300

    test: modify test funtion to test neovim debug

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -22,7 +22,9 @@ func TestString(t *testing.T) {
 		},
 	}
 
-	if program.String() != "let myVar = anotherVar;" {
+	representation := program.String()
+
+	if representation != "let myVar = anotherVar;" {
 		t.Errorf("program.String wrong. got=%q", program.String())
 	}
 }
