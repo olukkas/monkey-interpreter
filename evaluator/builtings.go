77aceafbc55a31edeb8f20d34782e22b4676a30b
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-06-11 15:36:33 -0300

    feat: builting function for arrays
    adding suport for arrays in len function
    and implementing some array specific functions
    
    first: return the first element
    last: return the last element
    rest: return all the elemets past the first one
    push: adds a new element to array

--- evaluator/builtings.go
+++ evaluator/builtings.go
@@ -2,16 +2,32 @@ package evaluator
 
 import "monkey/object"
 
+func wronElementesError(expected, length int) object.Object {
+	return object.NewErrorObject("wrong number of arguments. got=%d, wat=1", length, expected)
+}
+
+func wrongTypeError(funcName, expected, got string) object.Object {
+	return object.NewErrorObject(
+		"argument to `%s` must be %s, got %s",
+		funcName, expected, got,
+	)
+}
+
 var builtings = map[string]*object.Builting{
 	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
-				return object.NewErrorObject("wrong number of arguments. got=%d, wat=1", len(args))
+				return wronElementesError(1, len(args))
 			}
 
 			switch arg := args[0].(type) {
 			case *object.String:
-				return object.NewIntegerObject(int64(len(arg.Value)))
+				strLen := len(arg.Value)
+				return object.NewIntegerObject(int64(strLen))
+
+			case *object.Array:
+				elmentsLen := len(arg.Elements)
+				return object.NewIntegerObject(int64(elmentsLen))
 
 			default:
 				return object.NewErrorObject(
@@ -19,6 +35,89 @@ var builtings = map[string]*object.Builting{
 					args[0].Type(),
 				)
 			}
+		},
+	},
+
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return wronElementesError(1, len(args))
+			}
+
+			if args[0].Type() != object.ArrayObj {
+				return wrongTypeError("first", "ARRAY", string(args[0].Type()))
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return Null
+		},
+	},
+
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return wronElementesError(1, len(args))
+			}
+
+			if args[0].Type() != object.ArrayObj {
+				return wrongTypeError("last", "ARRAY", string(args[0].Type()))
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[length-1]
+			}
+
+			return Null
+		},
+	},
+
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return wronElementesError(1, len(args))
+			}
+
+			if args[0].Type() != object.ArrayObj {
+				return wrongTypeError("rest", "ARRAY", string(args[0].Type()))
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, arr.Elements[1:length])
+
+				return object.NewArray(newElements)
+			}
+
+			return Null
+		},
+	},
+
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return wronElementesError(2, len(args))
+			}
+
+			if args[0].Type() != object.ArrayObj {
+				return wrongTypeError("push", "ARRAY", string(args[0].Type()))
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+
+			return object.NewArray(newElements)
 		},
 	},
 }
