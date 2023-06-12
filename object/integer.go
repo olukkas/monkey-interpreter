4d10d5940c040955d87c66de9ab028766495894d
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-06-11 22:16:28 -0300

    feat(object): implement hashes
    hashes for strings, booleans adn integer
    for be used to construct monkey hashes

--- object/integer.go
+++ object/integer.go
@@ -10,5 +10,17 @@ func NewInteger(value int64) *Integer {
 	return &Integer{Value: value}
 }
 
-func (i *Integer) Type() ObjectType { return IntegerObj }
-func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType {
+	return IntegerObj
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{
+		Type:  i.Type(),
+		Value: uint64(i.Value),
+	}
+}
