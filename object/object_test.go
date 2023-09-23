d1758ff4ff7dda61f2c2539a27e0510e0cd0205a
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-09-23 00:52:42 -0300

    refactor: refactoring object_test.go.go to use testify lib

--- object/object_test.go
+++ object/object_test.go
@@ -1,41 +1,58 @@
 package object
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestStringHashKey(t *testing.T) {
+	asserts := assert.New(t)
+
 	hello1 := NewString("Hello World")
 	hello2 := NewString("Hello World")
 
 	diff1 := NewString("My name is Jhon")
 	diff2 := NewString("My name is Jhon")
 
-	if hello1.HashKey() != hello2.HashKey() {
-		t.Errorf("strings with same content have different hash keys")
-	}
+	asserts.Equal(
+		hello1.HashKey(),
+		hello2.HashKey(),
+		"strings with same content have different hash keys",
+	)
 
-	if diff1.HashKey() != diff2.HashKey() {
-		t.Errorf("strings with same content have different hash keys")
-	}
+	asserts.Equal(
+		diff1.HashKey(),
+		diff2.HashKey(),
+		"strings with same content have different hash keys",
+	)
 
-	if hello1.HashKey() == diff1.HashKey() {
-		t.Errorf("strings with different content have same hash keys")
-	}
+	asserts.NotEqual(
+		hello1.HashKey(),
+		diff1.HashKey(),
+		"strings with different content have same hash keys",
+	)
 }
 
 func TestIntegerHashKey(t *testing.T) {
 	one := NewInteger(1)
 	two := NewInteger(2)
 
-	if one.HashKey() == two.HashKey() {
-		t.Errorf("integers with different content have same hash keys")
-	}
+	assert.NotEqual(
+		t,
+		one.HashKey(),
+		two.HashKey(),
+		"integers with different content have same hash keys",
+	)
 }
 
 func TestBooleanHashKey(t *testing.T) {
 	bTrue := NewBoolean(true)
 	bFalse := NewBoolean(false)
 
-	if bTrue.HashKey() == bFalse.HashKey() {
-		t.Errorf("booleans with different content have same hash keys")
-	}
+	assert.NotEqual(
+		t,
+		bTrue.HashKey(),
+		bFalse.HashKey(),
+		"booleans with different content have same hash keys",
+	)
 }
