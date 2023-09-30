faa0c5fd2023a02d63343da81bd4137ee63cbfd3
Lucas Martins dos Santos <msantos.lms@pf.gov.br>
2023-09-30 00:09:54 -0300

    fix: fixing orthographic error

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -8,7 +8,7 @@ import (
 type Lexer struct {
 	input        string
 	position     int  // current position input (points to current char)
-	readPosition int  // current reading position in input (after curret char)
+	readPosition int  // current reading position in input (after current char)
 	ch           byte // current char under examination
 }
 
